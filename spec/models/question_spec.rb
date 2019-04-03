d66a2881f92bf198a1d3cfcf0be725385a9e9321
Dastan Abeuov <d.abeuov9@gmail.com>
2019-04-03 11:08:39 +0600

    first commit 1_tdd validates title, body presence true

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+
+=begin
+  it 'validate precense of title' do
+    expect(Question.new(title: 'Mytitle')).to_not be_valid 
+  end
+
+  it 'validate precense of body' do
+    expect(Question.new(body: 'Mytext')).to_not be_valid 
+  end
+=end
+
+end
