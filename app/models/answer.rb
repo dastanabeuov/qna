ad5ea929dbac019f0f0e928a005bbbd7516a87a6
Dastan Abeuov <d.abeuov9@gmail.com>
2019-04-03 20:12:12 +0600

    delete errors files from spec and model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
 
-	belongs_to :question
+  belongs_to :question
 
   validates :body, presence: true
 
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
 
+  it { should have_many(:answers).dependent(:destroy) }
+
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-
-  it { should have_many(:answers).dependent(:destroy) }
 
 =begin
   it 'validate precense of title' do
