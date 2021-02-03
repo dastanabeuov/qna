44970d5bd04559168035e5f8d16db1a1264ed28b
Dastan Abeuov <d.abeuov9@@gmail.com>
2021-02-03 23:54:08 +0600

    OmniAuth2+ last version integrate GitHub and Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,16 @@ class User < ApplicationRecord
 	has_many :comments
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[github facebook]
+
+  def self.find_for_oauth(auth)
+    Services::FindForOauth.new(auth).call
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
+  end
 
   def author_of?(resource)
     resource.user_id == id
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,4 +21,16 @@ RSpec.describe User, type: :model do
       expect(invalid_user).to_not be_author_of(question)
     end
   end
+
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+    let(:service) { double('Services::FindForOauth') }
+
+    it 'calls Services::FindForOauth' do
+      expect(Services::FindForOauth).to receive(:new).with(auth).and_return(service)
+      expect(service).to receive(:call)
+      User.find_for_oauth(auth)
+    end
+  end
 end
